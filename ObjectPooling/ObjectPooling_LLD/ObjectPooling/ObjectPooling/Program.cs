23172cf9e1405ceb4f6b0653a227638948effd50
Ashirbad Nayak <ashirbadnayakofficial97@gmail.com>
2025-04-08 07:43:10 +0530

    resource pool manager

---
+++ ObjectPooling/ObjectPooling_LLD/ObjectPooling/ObjectPooling/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ObjectPooling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourcePoolManager resourcePoolManager = ResourcePoolManager.GetInstance(5);
+            Resource resource1 = resourcePoolManager.GetResource();
+            Resource resource2 = resourcePoolManager.GetResource();
+            Resource resource3 = resourcePoolManager.GetResource();
+            Resource resource4 = resourcePoolManager.GetResource();
+            Resource resource5 = resourcePoolManager.GetResource();
+            Resource resource6 = resourcePoolManager.GetResource();
+            resourcePoolManager.ReleaseResource(resource1);
+            resourcePoolManager.ReleaseResource(resource2);
+            resourcePoolManager.ReleaseResource(resource3);
+            resourcePoolManager.ReleaseResource(resource4);
+            resourcePoolManager.ReleaseResource(resource5);
+            resourcePoolManager.ReleaseResource(resource6);
+
+            // Simulate concurrent access
+            Parallel.For(0, 6, i =>
+            {
+              
+                    Console.WriteLine($"Thread {i} requesting resource...");
+                    Resource resource = resourcePoolManager.GetResource();
+                    if (resource == null)
+                    {
+                        Console.WriteLine($"Thread {i} could not get resource./Resource not available.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Thread {i} got resource.");
+                    }
+                    if (resource != null)
+                    {
+                        // Simulate some work with the resource
+                        Task.Delay(100).Wait();
+                        resourcePoolManager.ReleaseResource(resource);
+                        Console.WriteLine($"Thread {i} released resource.");
+                    }
+            });
+
+            // Wait for user input to keep the console open
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjectPooling/ObjectPooling_LLD/ObjectPooling/ObjectPooling/ResourcePoolManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPooling
+{
+    public class ResourcePoolManager
+    {
+        int _maxPoolSize;
+        List<Resource> _resourcePool = new List<Resource>();
+        List<Resource> _inUseResourcePool = new List<Resource>();
+        private static readonly Object _lock = new Object();
+        private static ResourcePoolManager _instance;
+        private ResourcePoolManager(int _initialPoolSize) { 
+            for(int i= 0; i < _initialPoolSize; i++)
+            {
+                Resource resource = new Resource();
+                _resourcePool.Add(resource);
+            }
+        }
+        public static ResourcePoolManager GetInstance(int _initialPoolSize)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ResourcePoolManager(_initialPoolSize);
+                    }
+                }
+            }
+            return _instance;
+        }
+        public Resource GetResource()
+        {
+            lock (_lock)
+            {
+                if (_resourcePool.Count > 0)
+                {
+                    Resource resource = _resourcePool[0];
+                    _resourcePool.RemoveAt(0);
+                    _inUseResourcePool.Add(resource);
+                    Console.WriteLine("Resource allocated");
+                    return resource;
+                }
+                else
+                {
+                    Console.WriteLine("No resources available");
+                    return null;
+                }
+            }
+        }
+        public void ReleaseResource(Resource resource)
+        {
+            lock (_lock)
+            {
+                if (_inUseResourcePool.Contains(resource))
+                {
+                    _inUseResourcePool.Remove(resource);
+                    _resourcePool.Add(resource);
+                    Console.WriteLine("Resource released");
+                }
+                else
+                {
+                    Console.WriteLine("No resources available");
+                }
+            }
+        }
+
+
+    }
+}
